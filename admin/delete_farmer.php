9547f419581bbbccd325a088fe6194f216c8dddf
jocmpo <93925138+jocmpo@users.noreply.github.com>
2024-11-21 12:28:31 +0800

    Delete admin directory

--- admin/delete_farmer.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../connection/connect.php");
-error_reporting(0);
-session_start();
-
-mysqli_query($db,"DELETE FROM farmer WHERE f_id = '".$_GET['f_del']."'");
-header("location:all_farmer.php");  
-
-?>
--- admin/delete_product.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../connection/connect.php");
-error_reporting(0);
-session_start();
-
-mysqli_query($db,"DELETE FROM products WHERE p_id = '".$_GET['product_del']."'");
-header("location:all_product.php");  
-
-?>
